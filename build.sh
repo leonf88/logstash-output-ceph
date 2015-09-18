899102e130847bf0e5422c8e5f55d7ee3e697b0b
Fan Liang <chcdlf@gmail.com>
2015-09-18 15:47:01 +0800

    update build script

--- build.sh
+++ build.sh
@@ -11,6 +11,14 @@ ${GEMSPEC}
 "lib"
 )
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
 if [ "`which jrubyc`" = "" ]; then
     echo "To build the project, you must have jrubyc in the path."
     exit -1
@@ -22,11 +30,23 @@ fi
 
 mkdir ${BUILD_DIR}
 
-for f in $INCLUDE_FILES; do
+for f in ${INCLUDE_FILES[@]}; do
     cp -r $f ${BUILD_DIR}
 done
 
 pushd ${BUILD_DIR}
-    find . -name "*.rb" -exec jrubyc {} \;
-    gem build ${GEMSPEC}
+    find . -name "*.rb" | xargs -I{} sh -c "jrubyc {}; rm {}"
+    [ "`which tree`" != "" ] && tree
+    echo "# build the gem" > build.log
+    gem build ${GEMSPEC} &>> build.log
+    GEMFILE=`ls *gem`
+    mv ${GEMFILE} build.log ..
 popd
+
+if [ -f "$GEMFILE" ]; then
+    echo -e "\033[32mHave generate the gem file: $GEMFILE\033[0m"
+else
+    echo -e "\033[31mFailed to generate the gem file, you may check the build.log.\033[0m"
+fi
+
+rm ${BUILD_DIR} -rf
